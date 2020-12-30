7afc691105b11e00d8782996ec87ec6ad4956baf
BennoAlif <bennoalif41@gmail.com>
2020-12-30 17:27:30 +0700

    transaksi

--- app/Http/Controllers/Webhook.php
+++ app/Http/Controllers/Webhook.php
@@ -182,7 +182,7 @@ class Webhook extends Controller
                 ],
             ]);
         } else if (strtolower($userMessage) == 'pemasukan' || strtolower($userMessage) == 'pengeluaran') {
-            $this->addTransactions($userMessage, $event['replyToken']);
+            // $this->addTransactions($userMessage, $event['replyToken']);
 
             $message = "Silahkan ketik nominal ${userMessage}nya, kak!";
 
